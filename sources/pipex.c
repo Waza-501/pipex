e3b1eca0b0277b2768e3136ff84e11720d2993c6
Owen <you@example.comowen.d.hearn@gmail.com>
2025-04-04 16:07:36 +0200

    -

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,8 +6,13 @@
 /*   By: owhearn <owhearn@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/03 15:55:18 by owhearn       #+#    #+#                 */
-/*   Updated: 2025/04/03 15:55:26 by owhearn       ########   odam.nl         */
+/*   Updated: 2025/04/04 07:01:04 by owen          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+int	main(int argc, char **argv)
+{
+	
+}
