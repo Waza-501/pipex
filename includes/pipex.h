422c200445ec05cce26635a7ae335e05f87e4b5c
owhearn <owen.d.hearn@gmail.com>
2025-04-07 17:21:16 +0200

    starting on parsing

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,12 +6,27 @@
 /*   By: owhearn <owhearn@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/03 15:55:40 by owhearn       #+#    #+#                 */
-/*   Updated: 2025/04/03 15:57:12 by owhearn       ########   odam.nl         */
+/*   Updated: 2025/04/07 16:46:16 by owhearn       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+
+# include "../libraries/libft/libft.h"
+# include <sys/types.h>
+# include <fcntl.h>
+
+# define X_INPUT	"Error. Wrong input was given."
+
+typedef struct s_data
+{
+	int		fd1;
+	int		fd2;
+	int		pid1;
+	int		pid2;
+
+}			t_data;
 
 
 #endif
--- sources/pipex.c
+++ sources/pipex.c
@@ -6,13 +6,38 @@
 /*   By: owhearn <owhearn@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/03 15:55:18 by owhearn       #+#    #+#                 */
-/*   Updated: 2025/04/04 07:01:04 by owen          ########   odam.nl         */
+/*   Updated: 2025/04/07 16:54:23 by owhearn       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+int	parse_input(t_data data, char **argv, char **envp)
 {
-	
+	data.fd1 = open(argv[1], O_RDONLY);
+	data.fd2 = open(argv[4], O_RDONLY);
+	if (data.fd1 == -1 || data.fd2 == -1)
+		error_exit("haha funny can't open file");
 }
+
+int	main(int argc, char **argv, char **envp)
+{
+	pid_t	mario;
+	pid_t	luigi;
+	t_data	data;
+
+	if (argc != 5)
+	{
+		ft_putendl_fd("Error. Expected: ./pipex file1 cmd1 cmd2 file2", 2);
+		exit(-1);
+	}
+	parse_input(data, argv, envp);
+	return (0);
+}
+
+/*some notes
+When the command its not found, return 127 When the command is found, but not
+executable, return 126 Remember to call WEXITSTATUS in the parent This is up to
+you but I would advise handling path commands too (like ./script.sh)
+Be careful to close all opened file descriptors.  It's usefull to only open
+fds in the child process that actually needs them (Where possible)*/
