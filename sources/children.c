237712e4c664aba6038d90c77c61b19522e72d6e
Owen <you@example.comowen.d.hearn@gmail.com>
2025-04-15 13:06:00 +0200

    leak fix in error messages

--- sources/children.c
+++ sources/children.c
@@ -6,7 +6,7 @@
 /*   By: owen <owen@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/10 12:19:50 by owen          #+#    #+#                 */
-/*   Updated: 2025/04/15 12:38:36 by owen          ########   odam.nl         */
+/*   Updated: 2025/04/15 13:05:45 by owen          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	find_valid_cmd(t_data *data, char **cmd, char *copy, int i)
 	char	*temp;
 
 	temp = NULL;
+	if (!copy)
+		exit_error(NULL, 1, data);
 	while (data->paths[i])
 	{
 		if (access(*cmd, F_OK) == 0)
@@ -35,27 +37,23 @@ int	find_valid_cmd(t_data *data, char **cmd, char *copy, int i)
 	return (i);
 }
 
-void	set_cmd_path(t_data *data, char **cmd)
+void	set_cmd_path(t_data *data, char **cmd, char **copy)
 {
-	char	*copy;
 	bool	cmd_found;
 	int		i;
 
-	copy = ft_strdup(*cmd);
-	if (!copy)
-		exit_error(NULL, 1, data);
 	cmd_found = false;
 	i = 0;
 	while (access(*cmd, X_OK) != 0 && data->paths[i])
 	{
-		i = find_valid_cmd(data, cmd, copy, i);
+		i = find_valid_cmd(data, cmd, ft_strdup(copy[0]), i);
 		if (data->paths[i])
 				cmd_found = true;
 	}
 	if (access(*cmd, X_OK) != 0 && cmd_found == true)
-		exit_error(ft_strjoin(copy, ERR_126), 126, data);
+		exit_error(ft_strjoin(copy[0], ERR_126), 126, data);
 	else if (access(*cmd, X_OK) != 0)
-		exit_error(ft_strjoin(copy, ERR_127), 127, data);
+		exit_error(ft_strjoin(copy[0], ERR_127), 127, data);
 	free(data->paths);
 	free(copy);
 }
@@ -69,7 +67,7 @@ void	child_1(t_data *data, char *file, char **envp)
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 		exit_error(NULL, 1, data);
-	set_cmd_path(data, &data->cmd_1);
+	set_cmd_path(data, &data->cmd_1, data->args_1);
 	if (!data->cmd_1)
 		exit_error(NULL, 1, data);
 	if (dup2(fd, STDIN_FILENO) == -1)
@@ -89,7 +87,7 @@ void	child_2(t_data *data, char *file, char **envp)
 	fd = open(file, O_CREAT | O_WRONLY| O_TRUNC, 0644);
 	if (fd == -1)
 		exit_error(NULL, 1, data);
-	set_cmd_path(data, &data->cmd_2);
+	set_cmd_path(data, &data->cmd_2, data->args_2);
 	if (!data->cmd_2)
 		exit_error(NULL, 1, data);
 	if (dup2(data->pipefd[0], STDIN_FILENO) == -1)
